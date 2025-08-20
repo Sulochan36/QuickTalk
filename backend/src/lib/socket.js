81b4f54897b736d07f629bd7388a11908303bb1a
Sulochan Mahajan <sulochanmahajan@gmail.com>
2025-08-20 09:31:37 +0530

    socket.js updated

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -4,10 +4,14 @@ import express from "express";
 
 const app = express();
 const server = http.createServer(app);
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://quick-talk-one.vercel.app/"
+];
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: allowedOrigins,
   },
 });
 
